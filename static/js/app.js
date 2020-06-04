05e607c3086b1ef18f65396b886a2f314e31a169
Joker_ <joker@mynook.info>
2020-06-04 15:53:53 +0800

    Add copy button to code blocks, ref: https://www.dannyguo.com/blog/how-to-add-copy-to-clipboard-buttons-to-code-blocks-in-hugo/

--- static/js/app.js
+++ static/js/app.js
@@ -1,3 +1,4 @@
+// MathJax config
 window.MathJax = {
   messageStyle: 'none',
   tex2jax: {
@@ -9,12 +10,53 @@ window.MathJax = {
 };
 (function(){
   var main = function(){
+    // ToC show/dismiss
     var toc = document.getElementById('toc');
     if (toc) {
       toc.querySelector('a.toc-dismiss').addEventListener('click', function(e) {
         e.preventDefault();
         toc.classList.toggle('dismissed');
       });
+    }
+
+    // Code block copying
+    function initCodeCopy(clipboard) {
+      document.querySelectorAll('pre>code').forEach(function(blk) {
+        var btn = document.createElement('button');
+        btn.className = 'copy-code';
+        btn.type = 'buton';
+        btn.innerText = 'Copy';
+
+        var pre = blk.parentNode;
+        // Skip line number area
+        if (blk.classList.length === 0 && pre.classList.contains('chroma') && pre.parentNode.classList.contains('lntd')) {
+          return;
+        }
+        // Find outtermost .highlight, insert before that
+        var container = pre.closest('.highlight');
+        container.parentNode.insertBefore(btn, container);
+        btn.addEventListener('click', function() {
+          clipboard.writeText(blk.innerText).then(function() {
+            btn.blur();
+            btn.innerText = 'Copied!';
+            setTimeout(function() { btn.innerText = 'Copy'; }, 2000);
+          }).catch(function() {
+            btn.innerText = 'Failed!';
+            setTimeout(function() { btn.innerText = 'Copy'; }, 2000);
+          });
+        });
+      });
+    }
+    // Use native clipboard API, or a polyfill
+    if (navigator && navigator.clipboard) {
+      initCodeCopy(navigator.clipboard);
+    } else {
+      var spt = document.createElement('script');
+      spt.src = 'https://cdn.jsdelivr.net/npm/clipboard-polyfill@2.8.6/dist/clipboard-polyfill.js';
+      spt.integrity = 'sha256-CXoEMsIBPFOiyA+SLBvD8s5NWUBEqNwOwmeTjHV7/NE=';
+      spt.crossOrigin = 'anonymous';
+      spt.onload = function() { initCodeCopy(clipboard); };
+      document.body.appendChild(spt);
     }
   };
   (document.readyState !== 'loading') ? (main()) : (
